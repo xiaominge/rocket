28e9d45d467dc2eaa502930f39234da091d72845
1337991560@qq.com <1337991560@qq.com>
2015-08-23 23:02:32 +0800

    修改目录名

--- app/functions/extend.php
+++ app/functions/extend.php
@@ -77,3 +77,42 @@ function in_weixin()
 
     return false;
 }
+
+/**
+ * 图片别名加载
+ * @param  dynamic  mixed  配置文件中的别名
+ * @return string
+ */
+function img()
+{
+    $configKey = 'extend.imgAliases';
+
+    $args = array_merge(array($configKey), func_get_args());
+    return call_user_func_array('\Madphp\Src\Core\View\Widget::img', $args);
+}
+
+/**
+ * 样式别名加载
+ * @param  dynamic  mixed  配置文件中的别名
+ * @return string
+ */
+function style()
+{
+    $configKey = 'extend.cssAliases';
+
+    $args = array_merge(array($configKey), func_get_args());
+    return call_user_func_array('\Madphp\Src\Core\View\Widget::style', $args);
+}
+
+/**
+ * 脚本别名加载
+ * @param  dynamic  mixed  配置文件中的别名
+ * @return string
+ */
+function script()
+{
+    $configKey = 'extend.jsAliases';
+
+    $args = array_merge(array($configKey), func_get_args());
+    return call_user_func_array('\Madphp\Src\Core\View\Widget::script', $args);
+}
